e423bafd4f147a0de2177a17d0f838f47d804066
Acolit <deepir@mail.ru>
2023-11-19 14:29:28 +0400

    Your commit message

---
+++ mail.php
@@ -0,0 +1,47 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+$title = "Заявка на консультация";
+$file = $_FILES['file'];
+
+$c = true;
+// Формирование самого письма
+$title = "Заявка на консультация";
+foreach ( $_POST as $key => $value ) {
+  if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
+    $body .= "
+    " . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+      <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+      <td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+    </tr>
+    ";
+  }
+}
+
+$body = "<table style='width: 100%;'>$body</table>";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+  $mail->CharSet = "UTF-8";
+  $mail->setFrom('sales@vds-school.ru', 'Заявка на консультация'); // Адрес самой почты и имя отправителя
+
+  // Получатель письма
+  $mail->addAddress('acollit9@gmail.com');
+  $mail->addAddress('deepir@mail.ru');
+
+
+  // Отправка сообщения
+  $mail->isHTML(true);
+  $mail->Subject = $title;
+  $mail->Body = $body;
+
+  $mail->send();
+
+} catch (Exception $e) {
+  $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
